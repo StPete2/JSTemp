e3a81c24375fb15f450e9d25aee6479dccdb344e
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2024-07-12 15:20:05 +0300

    hw2

--- HWS1_onlyFirstTask/task1.js
+++ HWS1_onlyFirstTask/task1.js
@@ -1,33 +1,65 @@
 // Задание 1
-// • Используя Symbol.iterator, создайте объект "Музыкальная коллекция", который можно итерировать. Каждая итерация должна возвращать следующий альбом из коллекции.
+// Представьте, что у вас есть класс для управления библиотекой. В этом классе будет приватное свойство для хранения списка книг, а также методы для добавления книги, удаления книги и получения информации о наличии книги.
+// Класс должен содержать приватное свойство #books, которое инициализируется пустым массивом и представляет собой список книг в библиотеке.
+// Реализуйте геттер allBooks, который возвращает текущий список книг.
+// Реализуйте метод addBook(title), который позволяет добавлять книгу в список. Если книга с таким названием уже существует в списке, выбросьте ошибку с соответствующим сообщением.
+// Реализуйте метод removeBook(title), который позволит удалять книгу из списка по названию. Если книги с таким названием нет в списке, выбросьте ошибку с соответствующим сообщением.
+// Реализуйте метод hasBook(title), который будет проверять наличие книги в библиотеке и возвращать true или false в зависимости от того, есть ли такая книга в списке или нет.
+// Реализуйте конструктор, который принимает начальный список книг (массив) в качестве аргумента. Убедитесь, что предоставленный массив не содержит дубликатов; в противном случае выбрасывайте ошибку.
 
-// • Создайте объект musicCollection, который содержит массив альбомов и имеет свойство-символ Symbol.iterator. Каждый альбом имеет следующую структуру:
-
-// {
-// title: "Название альбома",
-// artist: "Исполнитель",
-// year: "Год выпуска"
-// }
-
-// • Реализуйте кастомный итератор для объекта musicCollection. Итератор должен перебирать альбомы по порядку.
-// • Используйте цикл for...of для перебора альбомов в музыкальной коллекции и вывода их на консоль в формате: Название альбома - Исполнитель (Год выпуска)
-
-const albums = [
-    { title: "Дорога в облака", artist: "Браво", year: "1994" },
-    { title: "На перекрестках весны", artist: "Браво", year: "1996" },
-];
-
-const musicCollection = {
-    albums: albums,
-    *[Symbol.iterator]() {
-        for (const album of albums) {
-            yield album;
+class Library {
+    #books = [];
+    constructor(books) {
+        if (books.length !== Array.from(new Set(books)).length) {
+            throw new Error("There are duplicates in the library");
         }
-    },
-};
+        this.#books = books;
+    }
 
-console.log(musicCollection);
-
-for (const album of musicCollection) {
-    console.log(`${album.title} - ${album.artist} (${album.year})`);
+    get allBooks() { // получение списка книг
+        return this.#books;
+    }
+    addBook(title) {
+        // добавление книги
+        if (this.#books.indexOf(title) !== -1) {
+            throw new Error(
+                "Can't add the book to the libarary as there is such already"
+            );
+        }
+        this.#books.push(title);
+        return this.#books;
+    }
+    removeBook(title) {
+        // удаление книги
+        if (this.#books.indexOf(title) === -1) {
+            throw new Error(
+                "Can't delete the book as there is no such book in the library"
+            );
+        }
+        this.#books = this.#books.filter(
+            (elem) => elem !== this.#books[this.#books.indexOf(title)]
+        );
+    }
+    hasBook(title) {
+        // проверка на наличие книги в библиотеки
+        if (this.#books.indexOf(title) === -1) {
+            return false;
+        }
+        return true;
+    }
 }
+
+const library = new Library([]);
+console.log(library.allBooks);
+library.addBook("Test Name");
+library.addBook("Test Name2");
+console.log(library.allBooks);
+library.removeBook("Test Name2");
+console.log(library.allBooks);
+console.log(library.hasBook("Test Name"));
+library.addBook("Test Name3");
+console.log(library.allBooks);
+
+const library2 = new Library(["Test book", "Test book3", "Test book2"]);
+console.log(library2.allBooks);
+
---
+++ HWS1_onlyFirstTask/task21.js
@@ -0,0 +1,137 @@
+// Задание 2
+// Вы разрабатываете систему отзывов для вашего веб-сайта. Пользователи могут оставлять отзывы, но чтобы исключить слишком короткие или слишком длинные сообщения, вы решаете установить некоторые ограничения.
+
+// Создайте HTML-структуру с текстовым полем для ввода отзыва, кнопкой для отправки и контейнером, где будут отображаться отзывы.
+
+// Напишите функцию, которая будет добавлять отзыв в контейнер с отзывами. Однако если длина введенного отзыва менее 50 или более 500 символов, функция должна генерировать исключение.
+
+// При добавлении отзыва, он должен отображаться на странице под предыдущими отзывами, а не заменять их.
+
+// Вы можете использовать этот массив initialData для начальной загрузки данных при запуске вашего приложения.
+
+const divContainerReviewsEl = document.querySelector("div.container-reviews");
+const reviewsData = [
+    {
+        product: "Apple iPhone 13",
+        reviews: [
+            {
+                id: "1",
+                text: "Отличный телефон! Батарея держится долго.",
+            },
+            {
+                id: "2",
+                text: "Камера супер, фото выглядят просто потрясающе.",
+            },
+        ],
+    },
+    {
+        product: "Samsung Galaxy Z Fold 3",
+        reviews: [
+            {
+                id: "3",
+                text: "Интересный дизайн, но дорогой.",
+            },
+        ],
+    },
+    {
+        product: "Sony PlayStation 5",
+        reviews: [
+            {
+                id: "4",
+                text: "Люблю играть на PS5, графика на высоте.",
+            },
+        ],
+    },
+];
+console.log(reviewsData);
+
+const deviceNameEl = document.querySelector("h3.device-name");
+const reviewsBoxEl = document.querySelector("div.reviews-box");
+const reviewEl = document.querySelector("div.review");
+const reviewNumberEl = document.querySelector("p.review-number");
+const reviewTextEl = document.querySelector("p.review-text");
+const divDeviceEl = document.querySelector("div.device");
+// console.log(deviceNameEl);
+// console.log(reviewsBoxEl);
+// console.log(reviewEl);
+// console.log(reviewNumberEl);
+// console.log(reviewTextEl);
+
+reviewsData.forEach((element) => {
+    const deviceNameElInside = document.createElement("h3");
+    deviceNameElInside.innerHTML = element.product;
+    console.log(element);
+    const divDeviceElInside = document.createElement("div");
+    divDeviceElInside.appendChild(deviceNameElInside);
+    if (element.reviews.length > 1) {
+        element.reviews.forEach((reviewsElem) => {
+            const reviewNumberElInside = document.createElement("p");
+            reviewNumberElInside.innerHTML = reviewsElem.id;
+            divDeviceElInside.appendChild(reviewNumberElInside);
+            const reviewTextElInside = document.createElement("p");
+            reviewTextElInside.innerHTML = reviewsElem.text;
+            divDeviceElInside.appendChild(reviewTextElInside);
+        });
+    } else {
+        const reviewNumberElInside = document.createElement("p");
+        reviewNumberElInside.innerHTML = element.reviews[0].id;
+        divDeviceElInside.appendChild(reviewNumberElInside);
+        const reviewTextElInside = document.createElement("p");
+        reviewTextElInside.innerHTML = element.reviews[0].text;
+        divDeviceElInside.appendChild(reviewTextElInside);
+    }
+    divDeviceElInside.classList.add(`${element.product.replaceAll(" ", "")}`);
+    divContainerReviewsEl.appendChild(divDeviceElInside);
+});
+
+// поймать данные, которые вводит пользователь
+let foundDeviceName = "";
+// console.log(newReviewNumber);
+
+const inputEl = document.querySelector("input.user-review");
+const buttonEl = document.querySelector("button.submit-button");
+
+const selectValueEl = document.querySelector("select.list-of-devices");
+selectValueEl.addEventListener("change", () => {
+    foundDeviceName = selectValueEl.value;
+});
+
+buttonEl.addEventListener("click", () => {
+    const newReviewNumberEl = document.createElement("p");
+    const newReviewTextEl = document.createElement("p");
+    newReviewTextEl.innerHTML = inputEl.value;
+    if (inputEl.value.length < 3 || inputEl.value.length > 10) {
+        throw new Error("Отзыв не может быть меньше 3 и более 10 символов");
+    }
+    let newReviewNumber = 0;
+    reviewsData.forEach((el) => {
+        newReviewNumber += el.reviews.length;
+    });
+    reviewsData.forEach((element) => {
+        if (foundDeviceName === element.product) {
+            element.reviews.push({
+                id: `${newReviewNumber + 1}`,
+                text: inputEl.value,
+            });
+            console.log(reviewsData);
+            newReviewNumberEl.innerHTML = newReviewNumber + 1;
+
+            let divContainerNextElement =
+                divContainerReviewsEl.firstElementChild;
+            for (let i = 0; i < divContainerReviewsEl.children.length; i++) {
+                // console.log(divContainerNextElement);
+                if (
+                    divContainerNextElement.classList.contains(
+                        `${element.product.replaceAll(" ", "")}`
+                    )
+                ) {
+                    console.log("bingo");
+                    divContainerNextElement.appendChild(newReviewNumberEl);
+                    divContainerNextElement.appendChild(newReviewTextEl);
+                }
+                divContainerNextElement =
+                    divContainerNextElement.nextElementSibling;
+            }
+        }
+    });
+});
